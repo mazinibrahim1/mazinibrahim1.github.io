286e3d7ebaca9b1c3436f9264cba7deba059708d
mazinibrahim1 <mazinnv11@gmail.com>
2021-11-14 00:26:24 +0530

    Add files via upload

---
+++ calucator.js
@@ -0,0 +1,28 @@
+ function add() {
+    var q =document.getElementById("fno").value;
+    var w =document.getElementById("sno").value;
+    var p=Number(q)
+    var u=Number(w)
+    document.getElementById("answer").innerHTML=p+u
+  }
+function sub() {
+    var q =document.getElementById("fno").value;
+    var w =document.getElementById("sno").value;
+    var p=Number(q)
+    var u=Number(w)
+    document.getElementById("answer").innerHTML=p-u
+  }
+function multiply() {
+    var q =document.getElementById("fno").value;
+    var w =document.getElementById("sno").value;
+    var p=Number(q)
+    var u=Number(w)
+    document.getElementById("answer").innerHTML=p*u
+  }
+function divide() {
+    var q =document.getElementById("fno").value;
+    var w =document.getElementById("sno").value;
+    var p=Number(q)
+    var u=Number(w)
+    document.getElementById("answer").innerHTML=p/u
+  }
